3614490195f4d9d25b233197b9d0e6326a5f98a5
lulin-song <lulin.song@gmail.com>
2019-07-02 15:36:08 -0400

    Update schema_updates.sql

--- sql/schema_updates.sql
+++ sql/schema_updates.sql
@@ -1,2 +1,3 @@
 -- Increase the name column maximum character length from 50 to 256
-ALTER TABLE hudson.riverlife.chemistry ALTER COLUMN "name" TYPE varchar(256)
+ALTER TABLE hudson.riverlife.chemistry ALTER COLUMN "name" TYPE varchar(256);
+ALTER TABLE hudson.riverlife.physical_measurement ALTER COLUMN weather_last_3_days TYPE varchar(1024);
